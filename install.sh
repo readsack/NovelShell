75cfdda90206eb16a0deb5dfed2ecd131d9d4e51
readsack <agarwala.keshav10@gmail.com>
2026-05-10 00:06:59 +0530

    Changes to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+mkdir -p ~/.config/quickshell
 cp -r * ~/.config/quickshell
-sudo pacman -S qt6 qt5 git
+sudo pacman -S qt6 qt5 git --noconfirm
 git clone https://aur.archlinux.org/quickshell-git.git
 cd quickshell-git
 makepkg -si
